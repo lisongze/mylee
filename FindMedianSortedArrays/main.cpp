d846149cc7c0195951676ba137d1ea0da7a65713
lisongze <lisongze@holomatic.com>
2019-11-21 21:34:43 +0800

    update

--- FindMedianSortedArrays/main.cpp
+++ FindMedianSortedArrays/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-class Test
+class Solution
 {
 public:
     double getMedia(const std::vector<int>& nums)
@@ -17,13 +17,26 @@ public:
                 return static_cast<double>(nums[nums.size() / 2 - 1] + nums[nums.size() / 2]) / 2.0;
             }
         }
-        else
-        {
-            return 0.0;
-        }
+        // else
+        // {
+        //     return 0.0;
+        // }
     }
     void getLmaxRmin(const std::vector<int>& nums, double& Lmax, double& Rmin)
     {
+        if (nums.size() > 0)
+        {
+            if (nums.size() % 2 == 1)
+            {
+                Lmax = static_cast<double>(nums[nums.size() / 2]);
+                Rmin = static_cast<double>(nums[nums.size() / 2]);
+            }
+            else
+            {
+                Lmax = static_cast<double>(nums[nums.size() / 2 - 1]);
+                Rmin = static_cast<double>(nums[nums.size() / 2]);
+            }
+        }
     }
     int getMediaIndex(const std::vector<int>& nums)
     {
@@ -48,15 +61,47 @@ public:
 
         if (nums1.empty() == false && nums2.empty() == false)
         {
-            double media1 = getMedia(nums1);
-            double media2 = getMedia(nums2);
+            double media1       = getMedia(nums1);
+            double media2       = getMedia(nums2);
+            int    media_index1 = getMediaIndex(nums1);
+            int    media_index2 = getMediaIndex(nums2);
+            double Lmax1, Rmin1;
+            double Lmax2, Rmin2;
+            getLmaxRmin(nums1, Lmax1, Rmin1);
+            getLmaxRmin(nums2, Lmax2, Rmin2);
+
+            if (Lmax1 <= Rmin2 && Lmax2 <= Rmin1)
+            {
+                return std::max(media1, media2);
+            }
+
+            if (Lmax1 > Rmin2)
+            {
+                std::vector<int> left1(nums1.begin(), nums1.begin() + media_index1);
+                std::vector<int> right2(nums2.begin() + media_index2, nums2.end());
+                return divideFind(left1, right2);
+            }
+
+            if (Lmax2 > Rmin1)
+            {
+                std::vector<int> right1(nums1.begin() + media_index1, nums1.end());
+                std::vector<int> left2(nums2.begin(), nums2.begin() + media_index2);
+                return divideFind(right1, left2);
+            }
         }
     }
     double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2)
     {
+        divideFind(nums1, nums2);
     }
 };
 
 int main()
 {
+    std::vector<int> nums1 = {1, 2, 4, 5, 7, 9};
+    std::vector<int> nums2 = {3, 6, 8};
+    Solution         solution;
+    double           media = solution.findMedianSortedArrays(nums1, nums2);
+    std::cout << media << std::endl;
+    return 0;
 }
