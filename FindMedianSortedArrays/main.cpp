ac094a5ef6c237e98176780bc6fb1aeebaccdeda
lisongze <lisongze@holomatic.com>
2019-11-22 21:01:08 +0800

    update

--- FindMedianSortedArrays/main.cpp
+++ FindMedianSortedArrays/main.cpp
@@ -37,7 +37,7 @@ public:
         {
             if (nums.size() % 2 == 1)
             {
-                Lmax = static_cast<double>(nums[nums.size() / 2]);
+                Lmax = static_cast<double>(nums[nums.size() / 2 - 1]);
                 Rmin = static_cast<double>(nums[nums.size() / 2]);
             }
             else
@@ -74,6 +74,7 @@ public:
 
         if (nums1.empty() == false && nums2.empty() == false)
         {
+            int    whole_len    = nums1.size() + nums2.size();
             double media1       = getMedia(nums1);
             double media2       = getMedia(nums2);
             int    media_index1 = getMediaIndex(nums1);
@@ -90,13 +91,23 @@ public:
 
             if (Lmax1 <= Rmin2 && Lmax2 <= Rmin1)
             {
-                return std::max(media1, media2);
+                if (whole_len % 2 == 1)
+                {
+                    return std::min(nums1[media_index1], nums2[media_index2]);
+                }
+                else
+                {
+                    return static_cast<double>(std::min(nums1[media_index1], nums2[media_index2]) +
+                                               std::max(nums1[media_index1 - 1], nums2[media_index2 - 1])) /
+                           2.0;
+                }
+                // return std::max(media1, media2);
             }
 
             if (Lmax1 > Rmin2)
             {
                 std::vector<int> left1(nums1.begin(), nums1.begin() + media_index1);
-                std::vector<int> right2(nums2.begin() + media_index2, nums2.end());
+                std::vector<int> right2(nums2.begin() + media_index2 + 1, nums2.end());
                 return divideFind(left1, right2);
             }
 
@@ -117,7 +128,7 @@ public:
 int main()
 {
     std::vector<int> nums1 = {1, 2, 4, 5, 7, 9};
-    std::vector<int> nums2 = {3, 6, 8};
+    std::vector<int> nums2 = {3, 3, 8};
     Solution         solution;
     double           media = solution.findMedianSortedArrays(nums1, nums2);
     std::cout << media << std::endl;
